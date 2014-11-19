03dddab8cca2c31d9e7940b033043d8dd4caa3c7
italosestilon <italosestilon@gmail.com>
2014-11-19 19:58:00 -0300

    Tela.

--- SistemaDeSugestaoDeCompras/src/br/ufc/sma/TelaAvaliacaoDeOfertas.java
+++ SistemaDeSugestaoDeCompras/src/br/ufc/sma/TelaAvaliacaoDeOfertas.java
@@ -1,21 +1,24 @@
 package br.ufc.sma;
 
 
-import java.awt.BorderLayout;
-import java.util.Iterator;
+import java.awt.BorderLayout; 
+import java.util.Collection;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import java.util.Collection;
+import javax.swing.table.DefaultTableModel;
 
 import br.ufc.sma.contractnet.AgenteIniciante;
 
 
 public class TelaAvaliacaoDeOfertas extends JFrame{
 	private AgenteIniciante agente;
+	
+	private Collection<Cupom> collectionCupons;
+	private DefaultTableModel defaul;
 	
 	public TelaAvaliacaoDeOfertas(AgenteIniciante agente) {
 		this.agente = agente;
@@ -26,19 +29,22 @@ public class TelaAvaliacaoDeOfertas extends JFrame{
 		initComponents();
 	}
 	
+	public TelaAvaliacaoDeOfertas(Collection<Cupom> cupons) {
+		this.collectionCupons = cupons;
+		getContentPane().setLayout(null);
+		this.setSize(760, 700);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		
+		initComponents();
+		this.setVisible(true);
+	}
+	
 	private void initComponents(){
-		Collection<Cupom> collectionCupons = this.agente.getCupons();
-		Object[][] dados = new Object[collectionCupons.size()][3]; 
+		//Collection<Cupom> collectionCupons = this.agente.getCupons();
+		Object[][] dados = new Object[collectionCupons.size()][6]; 
 		int index = 0;
-		for(Cupom cupom: collectionCupons){
-			dados[index][0] = new JCheckBox();
-			dados[index][1] = cupom.getNomeProduto();
-			dados[index][2] = String.format(".2f", cupom.getPrecoProduto());
-			dados[index][3] = cupom.getTipoProduto();
-			dados[index][4] = cupom.getVendedor().getName();
-			dados[index][5] = cupom.getData();
-			index++;
-		}
+		
 
 		String[] colunas = {"Selecionar", "Nome", "Valor", "Tipo do item", "Nome do vendedor", "Data"};
 		
@@ -49,6 +55,37 @@ public class TelaAvaliacaoDeOfertas extends JFrame{
 		
 		JTable table = new JTable(dados, colunas);
 		table.setBounds(0, 0, 700, 600);
+		
+		defaul = new DefaultTableModel(
+				new Object[][] {
+				{null, null, null, null, null, null},
+				},
+				colunas
+				) {
+				Class[] columnTypes = new Class[] {
+				Boolean.class, Object.class, Object.class, Object.class, Object.class, Object.class
+				};
+				public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+				}
+				};
+		
+				
+				for(Cupom cupom: collectionCupons){
+					
+					dados[index][0] = null;
+					dados[index][1] = cupom.getNomeProduto();
+					dados[index][2] = String.format(".2f", cupom.getPrecoProduto());
+					dados[index][3] = cupom.getTipoProduto();
+					dados[index][4] = cupom.getVendedor();
+					dados[index][5] = cupom.getData().get(cupom.getData().DAY_OF_MONTH);
+					
+					defaul.addRow(dados);
+					
+					index++;
+					
+				}
+		table.setModel(defaul);
 		
 		JScrollPane scrollbar = new JScrollPane(table);
 		scrollbar.setBounds(743, 32, 17, 536);
---
+++ SistemaDeSugestaoDeCompras/src/br/ufc/sma/TesteTelaDeOfertas.java
@@ -0,0 +1,28 @@
+package br.ufc.sma;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class TesteTelaDeOfertas {
+	
+	public static void main(String[] args) {
+		
+		List<Cupom> cupons = new ArrayList<Cupom>();
+		
+		Calendar calendar = Calendar.getInstance();
+		
+	
+		
+		
+		
+		
+		for(int i =0; i <= 10; i++){
+			cupons.add(new Cupom("viagem para",45.0,calendar,"viagem"));
+		}
+		
+		TelaAvaliacaoDeOfertas tela = new TelaAvaliacaoDeOfertas(cupons);
+		
+	}
+
+}
